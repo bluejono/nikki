1c810c3ff3bd9ea17aa52982dd80b1ccf8fef08b
João Nogueira <devjoaoluiz@gmail.com>
2026-01-28 22:30:07 -0300

    hotfix: message to test branch compatibility

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="bg-black h-full flex items-center justify-center">
-      <Text className="text-yellow-500 text-center">Opa</Text>
+      <Text className="text-yellow-500 text-center">Aoba</Text>
     </View>
   );
 }
